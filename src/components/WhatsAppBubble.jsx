e7edbaf356b58f31845b9e4133fd86ec16a21da3
saintcitto <saintcitto@icloud.com>
2025-10-20 21:45:18 +0700

    update: ubah template WhatsAppBubble dengan format ruangan KTV

--- src/components/WhatsAppBubble.jsx
+++ src/components/WhatsAppBubble.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-const WhatsAppBubble = () => {
-    const phoneNumber = "6285362962929";
-    const message = "Halo, saya ingin pesan Sonia Cafe.";
-    const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+const WhatsAppBubble = ({ room }) => {
+  const phoneNumber = "6285362962929";
+  const message = `Nomor Ruangan: KTV ${room}\nPesan apa dan jumlahnya berapa:`;
+  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
-    return (
-        <a
-            href={whatsappLink}
-            className="whatsapp-bubble"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Chat on WhatsApp"
-        >
-            <img 
-                src="images/whatsappicon.png"
-                alt="Chat di WhatsApp" 
-                className="whatsapp-icon-img" 
-            />
-        </a>
-    );
+  return (
+    <a
+      href={whatsappLink}
+      className="whatsapp-bubble"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Chat on WhatsApp"
+    >
+      <img
+        src="images/whatsappicon.png"
+        alt="Chat di WhatsApp"
+        className="whatsapp-icon-img"
+      />
+    </a>
+  );
 };
 
 export default WhatsAppBubble;
