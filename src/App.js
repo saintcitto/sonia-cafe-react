35709bdcd9413c1116e599c166e916862cdd84a8
saintcitto <saintcitto@icloud.com>
2025-10-21 16:16:39 +0700

    remove header component and related styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import AppHeader from './components/AppHeader';
 import Hero from './components/Hero';
 import Menu from './components/Menu';
 import OperationalHours from './components/OperationalHours';
@@ -10,7 +9,6 @@ import { menuData } from './menuData.js';
 function App() {
   return (
     <div className="app">
-      <AppHeader />
       <main className="main-content">
         <Hero />
         <Menu menuData={menuData} />
